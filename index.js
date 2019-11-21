65ecbb9a0b6ce369dfc2d8d6407a6400cffa2319
David Acosta <davidacosta25081@yahoo.com>
2019-11-21 21:24:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ class Circle {
   this.radius = radius * Math.PI * 2;
   }
 
-  set diameter(diameter) {
-    this.radius = diameter / 2;
-    this.diameter = diameter;
-  }
+  
 
   get diameter(){
     return `${this.radius}` * 2;
